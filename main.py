5ea14580d1a9a414f5de4ed68a5ae5f2291bf39a
Steve Taylor <steve@deployhub.com>
2023-03-09 12:29:16 -0700

    debug value too long Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ from fastapi import Body, FastAPI, HTTPException, Request, Response, status
 from pydantic import BaseModel
 from sqlalchemy import sql, create_engine
 from sqlalchemy.exc import OperationalError, StatementError, InterfaceError
+
 # Init Globals
-service_name = 'ortelius-ms-dep-pkg-cud'
+service_name = "ortelius-ms-dep-pkg-cud"
 db_conn_retry = 3
 
 # Init FastAPI
-app = FastAPI(
-    title=service_name,
-    description=service_name
-)
+app = FastAPI(title=service_name, description=service_name)
 
 
 # Init db connection
@@ -42,17 +40,18 @@ db_name = os.getenv("DB_NAME", "postgres")
 db_user = os.getenv("DB_USER", "postgres")
 db_pass = os.getenv("DB_PASS", "postgres")
 db_port = os.getenv("DB_PORT", "5432")
-validateuser_url = os.getenv('VALIDATEUSER_URL', None )
+validateuser_url = os.getenv("VALIDATEUSER_URL", None)
 
-if (validateuser_url is None):
-    validateuser_host = os.getenv('MS_VALIDATE_USER_SERVICE_HOST', '127.0.0.1')
+if validateuser_url is None:
+    validateuser_host = os.getenv("MS_VALIDATE_USER_SERVICE_HOST", "127.0.0.1")
     host = socket.gethostbyaddr(validateuser_host)[0]
-    validateuser_url = 'http://' + host + ':' + str(os.getenv('MS_VALIDATE_USER_SERVICE_PORT', 80))
+    validateuser_url = "http://" + host + ":" + str(os.getenv("MS_VALIDATE_USER_SERVICE_PORT", 80))
 
-url = requests.get('https://raw.githubusercontent.com/pyupio/safety-db/master/data/insecure_full.json')
+url = requests.get("https://raw.githubusercontent.com/pyupio/safety-db/master/data/insecure_full.json")
 safety_db = json.loads(url.text)
 
 engine = create_engine("postgresql+psycopg2://" + db_user + ":" + db_pass + "@" + db_host + ":" + db_port + "/" + db_name, pool_pre_ping=True)
+
 
 # health check endpoint
 class StatusMsg(BaseModel):
@@ -66,147 +65,145 @@ async def health(response: Response) -> StatusMsg:
         with engine.connect() as connection:
             conn = connection.connection
             cursor = conn.cursor()
-            cursor.execute('SELECT 1')
+            cursor.execute("SELECT 1")
             if cursor.rowcount > 0:
-                return {"status": 'UP', "service_name": service_name}
+                return {"status": "UP", "service_name": service_name}
             response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-            return {"status": 'DOWN'}
+            return {"status": "DOWN"}
 
     except Exception as err:
         print(str(err))
         response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-        return {"status": 'DOWN'}
+        return {"status": "DOWN"}
+
+
 # end health check
 
 # validate user endpoint
 
+
 def example(filename):
-    text = ''
-    with open(filename, 'r') as f:
+    text = ""
+    with open(filename, "r") as f:
         text = f.read()
     return text
+
 
 class Message(BaseModel):
     detail: str
 
 
-@app.post('/msapi/deppkg/cyclonedx')
-async def cyclonedx(request: Request, response: Response, compid: int, cyclonedx_json: dict = Body(...,example=example('cyclonedx.json'),description='JSON output from running CycloneDX')):
-    try:
-        result = requests.get(validateuser_url + "/msapi/validateuser", cookies=request.cookies)
-        if (result is None):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed")
-
-        if (result.status_code != status.HTTP_200_OK):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed status_code=" + str(result.status_code))
-    except Exception as err:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed:" + str(err)) from None
-
+@app.post("/msapi/deppkg/cyclonedx")
+async def cyclonedx(request: Request, response: Response, compid: int, cyclonedx_json: dict = Body(..., example=example("cyclonedx.json"), description="JSON output from running CycloneDX")):
+    json.dumps(cyclonedx_json)
     components_data = []
-    components = cyclonedx_json.get('components', [])
+    components = cyclonedx_json.get("components", [])
 
     # Parse CycloneDX BOM for licenses
-    bomformat = 'license'
-    for component in (components):
-        packagename = component.get('name')
-        packageversion = component.get('version', '')
-        purl = component.get('purl', '')
+    bomformat = "license"
+    for component in components:
+        packagename = component.get("name")
+        packageversion = component.get("version", "")
+        purl = component.get("purl", "")
         pkgtype = ""
-        if (':' in purl):
-            pkgtype = purl.split('/')[0][4:]
+        if ":" in purl:
+            pkgtype = purl.split("/")[0][4:]
 
-        summary = ''
-        license_url = ''
-        license_name = ''
-        licenses = component.get('licenses', None)
-        if (licenses is not None and len(licenses) > 0):
-            license = licenses[0].get('license',{})
-            if (license.get('id', None) is not None):
-                license_name = license.get('id')
-            elif (license.get('name', None) is not None):
-                license_name = license.get('name')
+        summary = ""
+        license_url = ""
+        license_name = ""
+        licenses = component.get("licenses", None)
+        if licenses is not None and len(licenses) > 0:
+            license = licenses[0].get("license", {})
+            if license.get("id", None) is not None:
+                license_name = license.get("id")
+            elif license.get("name", None) is not None:
+                license_name = license.get("name")
 
-            if (len(license_name) > 0):
-                license_url = 'https://spdx.org/licenses/' + license_name + '.html'
+            if len(license_name) > 0:
+                license_url = "https://spdx.org/licenses/" + license_name + ".html"
         component_data = (compid, packagename, packageversion, bomformat, license_name, license_url, summary, purl, pkgtype)
         components_data.append(component_data)
 
     return saveComponentsData(response, compid, bomformat, components_data)
 
-@app.post('/msapi/deppkg/spdx')
-async def spdx(request: Request, response: Response, compid: int, spdx_json: dict = Body(...,example=example('spdx.json'),description='JSON output from running SPDX')):
+
+@app.post("/msapi/deppkg/spdx")
+async def spdx(request: Request, response: Response, compid: int, spdx_json: dict = Body(..., example=example("spdx.json"), description="JSON output from running SPDX")):
     try:
         result = requests.get(validateuser_url + "/msapi/validateuser", cookies=request.cookies)
-        if (result is None):
+        if result is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed")
 
-        if (result.status_code != status.HTTP_200_OK):
+        if result.status_code != status.HTTP_200_OK:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed status_code=" + str(result.status_code))
     except Exception as err:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed:" + str(err)) from None
 
     components_data = []
-    components = spdx_json.get('packages', [])
+    components = spdx_json.get("packages", [])
 
     # Parse SPDX BOM for licenses
-    bomformat = 'spdx_json'
-    for component in (components):
-        packagename = component.get('name')
-        packageversion = component.get('versionInfo', '')
-        extpkgs = component.get('externalRefs', [])
+    bomformat = "spdx_json"
+    for component in components:
+        packagename = component.get("name")
+        packageversion = component.get("versionInfo", "")
+        extpkgs = component.get("externalRefs", [])
         purl = ""
         pkgtype = ""
 
         for pkgref in extpkgs:
-            reftype = pkgref.get('referenceType', None)
-            if ( reftype is not None and reftype == "purl"):
-                purl = pkgref.get('referenceLocator', '')
+            reftype = pkgref.get("referenceType", None)
+            if reftype is not None and reftype == "purl":
+                purl = pkgref.get("referenceLocator", "")
 
-                if (':' in purl):
-                    pkgtype = purl.split('/')[0][4:]
+                if ":" in purl:
+                    pkgtype = purl.split("/")[0][4:]
 
-        summary = ''
-        license_url = ''
-        license_name = ''
-        license = component.get('licenseDeclared')
-        if (license != 'NOASSERTION'):
+        summary = ""
+        license_url = ""
+        license_name = ""
+        license = component.get("licenseDeclared")
+        if license != "NOASSERTION":
             license_name = license
-            license_url = 'https://spdx.org/licenses/' + license_name + '.html'
+            license_url = "https://spdx.org/licenses/" + license_name + ".html"
         component_data = (compid, packagename, packageversion, bomformat, license_name, license_url, summary, purl, pkgtype)
         components_data.append(component_data)
 
     return saveComponentsData(response, compid, bomformat, components_data)
 
-@app.post('/msapi/deppkg/safety')
-async def safety(request: Request, response: Response, compid: int, safety_json: list = Body(...,example=example('safety.json'),description='JSON output from running safety')):
+
+@app.post("/msapi/deppkg/safety")
+async def safety(request: Request, response: Response, compid: int, safety_json: list = Body(..., example=example("safety.json"), description="JSON output from running safety")):
     result = requests.get(validateuser_url + "/msapi/validateuser", cookies=request.cookies)
-    if (result is None):
+    if result is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed")
 
-    if (result.status_code != status.HTTP_200_OK):
+    if result.status_code != status.HTTP_200_OK:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed status_code=" + str(result.status_code))
 
     components_data = []
-    bomformat = 'cve'
-    for component in (safety_json):
+    bomformat = "cve"
+    for component in safety_json:
         packagename = component[0]  # name
         packageversion = component[2]  # version
         summary = component[3]
         safety_id = component[4]  # cve id
-        cve_url = ''
+        cve_url = ""
         cve_name = safety_id
         cve_detail = safety_db.get(packagename, None)
-        if (cve_detail is not None):
+        if cve_detail is not None:
             for cve in cve_detail:
-                if (cve['id'] == 'pyup.io-' + safety_id):
-                    cve_name = cve['cve']
-                    if (cve_name.startswith('CVE')):
-                        cve_url = 'https://nvd.nist.gov/vuln/detail/' + cve_name
+                if cve["id"] == "pyup.io-" + safety_id:
+                    cve_name = cve["cve"]
+                    if cve_name.startswith("CVE"):
+                        cve_url = "https://nvd.nist.gov/vuln/detail/" + cve_name
                     break
 
         component_data = (compid, packagename, packageversion, bomformat, cve_name, cve_url, summary)
         components_data.append(component_data)
     return saveComponentsData(response, compid, bomformat, components_data)
+
 
 def saveComponentsData(response, compid, bomformat, components_data):
     try:
@@ -216,24 +213,29 @@ def saveComponentsData(response, compid, bomformat, components_data):
         # remove dups
         components_data = list(set(components_data))
 
-        #Retry logic for failed query
+        # Retry logic for failed query
         no_of_retry = db_conn_retry
-        attempt = 1;
+        attempt = 1
         while True:
             try:
                 with engine.connect() as connection:
                     conn = connection.connection
                     conn.set_session(autocommit=False)
                     cursor = conn.cursor()
-                    records_list_template = ','.join(['%s'] * len(components_data))
+                    records_list_template = ",".join(["%s"] * len(components_data))
 
                     # delete old licenses
-                    sqlstmt = 'DELETE from dm_componentdeps where compid=%s and deptype=%s'
-                    params = (compid, bomformat,)
+                    sqlstmt = "DELETE from dm_componentdeps where compid=%s and deptype=%s"
+                    params = (
+                        compid,
+                        bomformat,
+                    )
                     cursor.execute(sqlstmt, params)
 
                     # insert into database
-                    sqlstmt = 'INSERT INTO dm_componentdeps(compid, packagename, packageversion, deptype, name, url, summary, purl, pkgtype) VALUES {}'.format(records_list_template)
+                    sqlstmt = "INSERT INTO dm_componentdeps(compid, packagename, packageversion, deptype, name, url, summary, purl, pkgtype) VALUES {} ON CONFLICT ON CONSTRAINT dm_componentdeps_pkey DO NOTHING".format(
+                        records_list_template
+                    )
 
                     cursor.execute(sqlstmt, components_data)
 
@@ -249,13 +251,8 @@ def saveComponentsData(response, compid, bomformat, components_data):
             except (InterfaceError, OperationalError) as ex:
                 if attempt < no_of_retry:
                     sleep_for = 0.2
-                    logging.error(
-                        "Database connection error: {} - sleeping for {}s"
-                        " and will retry (attempt #{} of {})".format(
-                            ex, sleep_for, attempt, no_of_retry
-                        )
-                    )
-                    #200ms of sleep time in cons. retry calls
+                    logging.error("Database connection error: {} - sleeping for {}s" " and will retry (attempt #{} of {})".format(ex, sleep_for, attempt, no_of_retry))
+                    # 200ms of sleep time in cons. retry calls
                     sleep(sleep_for)
                     attempt += 1
                     continue
@@ -267,6 +264,7 @@ def saveComponentsData(response, compid, bomformat, components_data):
     except Exception as err:
         print(str(err))
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)) from None
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5003)
