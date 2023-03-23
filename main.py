e3bbb605b68aeabc1590e1c6780d791dea4c5563
Steve Taylor <steve@deployhub.com>
2023-03-23 13:34:45 -0600

    ignore login on branch and require msg (#36)
    Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ engine = create_engine("postgresql+psycopg2://" + db_user + ":" + db_pass + "@" 
 # health check endpoint
 class StatusMsg(BaseModel):
     status: str
-    service_name: Optional[str] = None
+    service_name: str
 
 
 @app.get("/health", tags=["health"])
